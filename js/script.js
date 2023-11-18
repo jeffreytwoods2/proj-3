e0353ebd0e18cf1c98b7b127bab4a33db5e80ab6
jeffreywoods <jeffreytwoods2@icloud.com>
2023-11-17 19:12:34 -0700

    Added theme and toggle button

--- js/script.js
+++ js/script.js
@@ -11,3 +11,51 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+var allBtns = document.querySelectorAll(".btn-holder");
+
+for (i = 0; i < allBtns.length; i++) {
+  var btn = allBtns[i];
+
+  btn.addEventListener("click", function () {
+    var allNodes = btn.children;
+
+    // find all childern and check them for add class and change checkbox state
+    for (j = 0; j < allNodes.length; j++) {
+      var node = allNodes[j];
+      var isActive;
+
+      // check for btn circle and change it's css class
+      if (node.classList.contains("btn-circle")) {
+        if (!node.classList.contains("active")) {
+          node.classList.add("active");
+          isActive = true;
+          let head = document.head;
+          let link = document.createElement("link");
+
+          link.type = "text/css";
+          link.rel = "stylesheet";
+          link.href = "css/theme.css";
+          link.id = "theme";
+
+          head.appendChild(link);
+        } else {
+          node.classList.remove("active");
+          isActive = false;
+          let sheet = document.getElementById("theme");
+          sheet.disabled = true;
+          sheet.parentNode.removeChild(sheet);
+        }
+      }
+
+      // check for check box and change it's state
+      if (node.classList.contains("checkbox")) {
+        if (isActive) {
+          node.checked = true;
+        } else {
+          node.checked = false;
+        }
+      }
+    }
+  });
+}
